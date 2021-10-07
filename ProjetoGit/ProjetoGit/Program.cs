fbcf4b0f94e7161fb22ee31416758540ecd50dd5
Victor Hugo <victor_nascimento1996@hotmail.com>
2021-10-07 00:15:56 -0300

    Primeiro commit

---
+++ ProjetoGit/ProjetoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

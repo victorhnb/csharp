745cbccbbed6002d2004148cfed30fce3a35ae89
Victor Hugo <victor_nascimento1996@hotmail.com>
2021-10-07 00:18:16 -0300

    Segundo commit

--- ProjetoGit/ProjetoGit/Program.cs
+++ ProjetoGit/ProjetoGit/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia");
         }
     }
 }

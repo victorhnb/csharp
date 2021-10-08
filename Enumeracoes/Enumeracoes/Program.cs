6c788313e2da946d0f1cae790434efe5e035fe05
Victor Hugo <victor_nascimento1996@hotmail.com>
2021-10-08 00:02:19 -0300

    Convertendo String para tipo Enum

--- Enumeracoes/Enumeracoes/Program.cs
+++ Enumeracoes/Enumeracoes/Program.cs
@@ -17,6 +17,14 @@ namespace Enumeracoes
 
             Console.WriteLine(order);
 
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+
+
 
         }
     }

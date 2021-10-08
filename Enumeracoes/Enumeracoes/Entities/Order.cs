5fc4ac42ec172fa859e2472d76b86a8ebf12cf55
Victor Hugo <victor_nascimento1996@hotmail.com>
2021-10-07 23:46:06 -0300

    Criando Enum OrderStatus e Classe Order

---
+++ Enumeracoes/Enumeracoes/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using Enumeracoes.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeracoes.Entities
+{
+    class Order
+    {
+
+
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public int MyProperty { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+
+    }
+}
---
+++ Enumeracoes/Enumeracoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

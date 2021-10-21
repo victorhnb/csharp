4f56316f8da83b536fd0a38093f774bc145b692d
Victor Hugo <victor_nascimento1996@hotmail.com>
2021-10-21 00:20:10 -0300

    Implementando os metodos na classe Worker

--- ExercicioComposicao/ExercicioComposicao/Worker.cs
+++ ExercicioComposicao/ExercicioComposicao/Worker.cs
@@ -1,5 +1,6 @@
 ﻿
 using ExercicioComposicao.Enums;
+using System.Collections.Generic;
 
 namespace ExercicioComposicao
 {
@@ -8,15 +9,26 @@ namespace ExercicioComposicao
         public string  Name { get; set; }
         public int BaseSalary { get; set; }
         public WorkerLevel Level { get; set; }
+        public Department Department { get; set; }
+
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker(string name, int baseSalary, WorkerLevel level, Department department)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+            Level = level;
+            Department = department;
+        }
 
         public void AddContract(HourContract contract)
         {
-
+            Contracts.Add(contract);
         }
 
         public void RemoveContract(HourContract contract)
         {
-
+            Contracts.Remove(contract);
         }
 
         public double Income(int year, int month)

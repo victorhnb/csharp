da1961de9436b50907fb659aa903031b0e8c99dd
Victor Hugo <victor_nascimento1996@hotmail.com>
2021-11-11 00:33:33 -0300

    Criando classe Post e Comment

---
+++ ExercicioComposicao2/ExercicioComposicao2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioComposicao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

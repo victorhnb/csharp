24bef07d6e07fa54a8139ed72eaa8298d61fbcd3
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-02-10 01:30:46 -0300

    Realizando UpCasting

---
+++ Heranca/Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+
+        public double InteresetRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interesetRate) : base(number, holder, balance)
+        {
+            InteresetRate = interesetRate;
+        }
+
+
+        public void updateBalance()
+        {
+            Balance += Balance * InteresetRate;
+        }
+    }
+}
--- Heranca/Heranca/Program.cs
+++ Heranca/Heranca/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Heranca.Entities;
+using System;
 
 namespace Heranca
 {
@@ -6,7 +7,14 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 500.0);
+
+
+            // UPCASTING 
+            Account acc1 = bacc;
+
         }
     }
 }

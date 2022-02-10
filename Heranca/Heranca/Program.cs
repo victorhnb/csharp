69880b89abd1e2c03f1bad6e781d6bac0b6b43dd
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-02-10 01:33:57 -0300

    Realizando UpCasting2

--- Heranca/Heranca/Program.cs
+++ Heranca/Heranca/Program.cs
@@ -9,11 +9,14 @@ namespace Heranca
         {
 
             Account acc = new Account(1001, "Alex", 0.0);
-            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 500.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
 
             // UPCASTING 
             Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new BusinessAccount(1003, "Anna", 0.0, 0.01);
+
 
         }
     }

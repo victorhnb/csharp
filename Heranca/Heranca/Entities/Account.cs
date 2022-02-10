64b4e97d94b81f2440f92646263ff80557cd172b
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-02-10 00:48:54 -0300

    Criando Classe e Superclasse

---
+++ Heranca/Heranca/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Heranca.Entities
+{
+    class Account
+    {
+
+        public int Number { get; set; }
+        public string Holder { get; set; }
+
+        public double Balance { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void withDraw (double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit (double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}

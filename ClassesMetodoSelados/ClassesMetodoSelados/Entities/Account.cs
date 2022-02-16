4dc7c8b563fd8fa32880be92623f6e2e4d4214ff
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-02-16 01:06:14 -0300

    Palavra sealed para classes e metodos selados

---
+++ ClassesMetodoSelados/ClassesMetodoSelados/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMetodoSelados.Entities
+{
+   sealed class Account
+    {
+
+        public int ReturnTest()
+        {
+            return 0;
+        }
+
+    }
+}

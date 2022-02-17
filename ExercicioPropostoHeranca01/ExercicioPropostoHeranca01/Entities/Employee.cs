e27bb835643da96403e5c711d7621171ade67d2c
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-02-17 00:28:50 -0300

    Iniciando classe Employee

---
+++ ExercicioPropostoHeranca01/ExercicioPropostoHeranca01/Entities/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPropostoHeranca01.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public int ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, int valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+    }
+}

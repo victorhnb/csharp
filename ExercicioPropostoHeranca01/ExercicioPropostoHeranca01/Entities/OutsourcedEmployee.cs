ab3c20244fbe1e40f611fcf7abe40338e91607f6
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-02-17 00:36:20 -0300

    Implementando classe OutsourcedEmployee

---
+++ ExercicioPropostoHeranca01/ExercicioPropostoHeranca01/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace ExercicioPropostoHeranca01.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+
+        public double additionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, int valuePerHour, double additionalcharge) : base(name, hours, valuePerHour)
+        {
+            additionalCharge = additionalcharge;
+
+        }
+
+
+
+    }
+}

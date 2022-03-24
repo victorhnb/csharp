44e9bfaa76aee10a685543d15991d30d3e4d9203
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-03-24 01:04:54 -0300

    Implementando a classe principal

--- ExePropostoHerancaPolimorfismo/ExePropostoHerancaPolimorfismo/Program.cs
+++ ExePropostoHerancaPolimorfismo/ExePropostoHerancaPolimorfismo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExePropostoHerancaPolimorfismo.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace ExePropostoHerancaPolimorfismo
 {
@@ -6,7 +8,43 @@ namespace ExePropostoHerancaPolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Product> list = new List<Product>;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Product #" + i + " data: ");
+                Console.Write("Common, used, or imported (c/u/i)?");
+                char producttype = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (producttype == 'i')
+                {
+                    Console.Write("Customs Free: ");
+                    double customsFree = double.Parse(Console.ReadLine());
+                    list.Add(new ImportedProduct(name, price, customsFree));
+                }
+
+                else if (producttype == 'u')
+                {
+                    Console.Write("Manufacture Date: ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                }
+
+                else
+                {
+                    list.Add(new Product(name,price));
+                }
+
+            }
+
         }
     }
 }

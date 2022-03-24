94b3e2cbeb926b37aba9de6892711ffdbbd36a50
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-03-24 00:40:46 -0300

    Finalizada classe ImportedProduct

--- ExePropostoHerancaPolimorfismo/ExePropostoHerancaPolimorfismo/Entities/ImportedProduct.cs
+++ ExePropostoHerancaPolimorfismo/ExePropostoHerancaPolimorfismo/Entities/ImportedProduct.cs
@@ -2,8 +2,20 @@
 {
     class ImportedProduct : Product
     {
-        public double customsFee { get; set; } 
+        public double customsFee { get; set; }
 
+        public ImportedProduct()
+        {
+        }
 
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price) 
+        {
+            this.customsFee = customsFee;
+        }
+
+        public double totalPrice()
+        {
+            return base.Price + customsFee;
+        }
     }
 }

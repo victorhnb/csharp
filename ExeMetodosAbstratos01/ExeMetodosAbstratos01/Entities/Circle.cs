d898d3e7888b201d53dab7be0cc5f4fae5eaacb9
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-03-30 23:47:38 -0300

    Classes Figuras criadas

---
+++ ExeMetodosAbstratos01/ExeMetodosAbstratos01/Entities/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeMetodosAbstratos01.Entities
+{
+    class Circle
+    {
+
+        public double radious { get; set; }
+
+        public Circle()
+        {
+        }
+
+        public Circle(double radious)
+        {
+            this.radious = radious;
+        }
+    }
+}
---
+++ ExeMetodosAbstratos01/ExeMetodosAbstratos01/Entities/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeMetodosAbstratos01.Entities
+{
+    class Rectangle
+    {
+
+        public double width { get; set; }
+        public double height { get; set; }
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+
+    }
+}

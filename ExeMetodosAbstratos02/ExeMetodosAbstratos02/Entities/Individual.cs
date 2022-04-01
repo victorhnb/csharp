a8fc5196bfb1d89e7887bca03c633df2fe64403c
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-04-01 00:58:28 -0300

    Implementando a classe Individual

---
+++ ExeMetodosAbstratos02/ExeMetodosAbstratos02/Entities/Individual.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeMetodosAbstratos02.Entities
+{
+    class Individual : Payer
+    {
+
+        public int HealthExpenditure { get; set; }
+
+        public Individual(string name, double anualIncome, int healthExpenditure) : base(name, anualIncome)
+        {
+            HealthExpenditure = healthExpenditure;
+        }
+
+        public override double TaxesPaid()
+        {
+            if (base.AnualIncome < 20000.00)
+            {
+                return 20000.00 * 0.15;
+            }
+
+            else
+            {
+                return (base.AnualIncome - 0.25) - (this.HealthExpenditure * 0.50);
+            }         
+        }
+
+
+    }
+}

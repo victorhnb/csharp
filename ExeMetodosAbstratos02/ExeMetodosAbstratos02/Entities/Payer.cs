c634445478ff9b3b833f25c1b600d2ed3d85654e
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-04-01 00:43:56 -0300

    Criando classe Payer

---
+++ ExeMetodosAbstratos02/ExeMetodosAbstratos02/Entities/Payer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeMetodosAbstratos02.Entities
+{
+    abstract class Payer
+    {
+        public string Name { get; set; }
+        public double  AnualIncome { get; set; }
+
+        protected Payer()
+        {
+        }
+
+        protected Payer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double TaxesPaid();
+    }
+}

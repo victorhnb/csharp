bac9a6d0e717c4e8ea79b6c1734d14126347662c
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-04-06 00:58:19 -0300

    Implementando classe Company

---
+++ ExeMetodosAbstratos02/ExeMetodosAbstratos02/Entities/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeMetodosAbstratos02.Entities
+{
+    class Company : Payer
+
+
+    {
+
+        public int Employee { get; set; }
+
+        public Company(string name, double anualIncome, int employee) : base(name,anualIncome)
+        {
+            Employee = employee;
+        }
+
+        public override double TaxesPaid()
+        {
+            if (Employee >= 25)
+            {
+                return base.AnualIncome * 0.14;
+            }
+            else
+            {
+                return base.AnualIncome * 0.16;
+            }
+        }
+    }
+}

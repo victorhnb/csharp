57fcc60d0cfdf81578a641e138836d03f6cc04ba
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-09-21 00:35:43 -0300

    Trabalhando com arquivos - File e FileInfo

---
+++ File-FileInfo/FileFileInfo/FileFileInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

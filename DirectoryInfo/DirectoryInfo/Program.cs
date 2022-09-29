74f85d6b0d7f661ec10d20c1c664d5b99bbff6c7
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-09-29 01:05:40 -0300

    Trabalhando com Directory e DirectoryInfo

---
+++ DirectoryInfo/DirectoryInfo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        string path = @"c:\temp\myfolder";
+
+        try
+        {
+            // lista os diretorios no path indicado
+            var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+            Console.WriteLine("FOLDERS: ");
+
+            foreach (string s in folders)
+            {
+                Console.WriteLine(s);
+            }
+            // lista os arquivos no path de origem informado informado
+            var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            Console.WriteLine();
+            Console.WriteLine("FILES: ");
+
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+
+            // cria um diretorio a partir do path de origem
+
+            Directory.CreateDirectory(path + @"\newfolder");
+
+
+
+        }
+        catch (IOException e)
+        {
+
+            Console.WriteLine("An error ocorrued.");
+            Console.WriteLine(e.Message);
+
+        }
+
+    }
+    }
+

0b4a15e1ad304e3d633ff3242e24e7cbca026d12
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-09-29 00:43:17 -0300

    Trabalhando com StreamWriter

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+
+            try
+            {
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+

0a8aed622916b6503b23582de39829139c79864f
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-09-29 01:15:51 -0300

    Trabalhando com a classe Path

---
+++ PathClass/PathClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace PathClass
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"c:\temp\myfolder\file1.txt";
+
+            // busca a pasta que se encontra o arquivo
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+
+            Console.WriteLine("GetDirectoryName: "  + Path.GetDirectoryName(path));
+
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+
+
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+
+
+        }
+    }
+}

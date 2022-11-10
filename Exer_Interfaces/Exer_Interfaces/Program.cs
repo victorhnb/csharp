2bdde6e11e24b3cee9c4b9bdcc1f3d4a5146bd66
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-11-10 00:36:44 -0300

    Iniciando exe de interfaces

---
+++ Exer_Interfaces/Exer_Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exer_Interfaces
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installment: ");
+            int numberInstallment = int.Parse(Console.ReadLine());  
+
+
+        }
+    }
+}

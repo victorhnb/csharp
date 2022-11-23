b3aa0de6439955fc250109aa1fbd985b8d99d568
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-11-22 23:45:56 -0300

    Commit final

---
+++ StreamWriter/ExercicioFixacao/ExercicioFixacao/OutPutFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao
+{
+     class OutPutFile
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public OutPutFile(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ StreamWriter/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace ExercicioFixacao
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string sourcePath = @"C:\temp\projects\StreamWriter\ExercicioFixacao\out\summary.csv";
+            string targetPath = @"C:\temp\projects\StreamWriter\ExercicioFixacao\out\summary2.csv";
+
+              //StreamWriter sw = null;
+
+            try
+            {
+
+                
+                
+                    string[] lines = File.ReadAllLines(sourcePath);
+                    using (StreamWriter sw = File.AppendText(targetPath))
+                    {
+
+                        foreach (var item in lines)
+                        {
+                            var fields = item.Split(',');
+
+                            var operation = Convert.ToDouble(fields[1]) * Convert.ToDouble(fields[2]);
+
+
+                            OutPutFile of = new OutPutFile(fields[0], operation);
+
+                            
+                           sw.Write(of.Name + "," + of.Price.ToString("F2", CultureInfo.InvariantCulture) + "\n");
+
+
+                        }
+                    }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           /* finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }*/
+
+        }
+    }
+}
